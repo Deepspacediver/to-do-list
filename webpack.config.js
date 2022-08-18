776bb81b71d6b22c6abf39605843dc10d2bd9380
deepspacediver <luka.koscik@gmail.com>
2022-08-18 20:04:54 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+ const HtmlWebpackPlugin = require('html-webpack-plugin') 
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.js',
+   plugins: [
+    new HtmlWebpackPlugin({
+        title:"To-do List",
+    }),
+  ], 
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+      rules: [{
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
