969f0cb22e50ed0296ec7139fdc2bb53b13c8814
deepspacediver <luka.koscik@gmail.com>
2022-08-23 16:11:11 +0200

    added logic to add newly created todo to an array

---
+++ src/formHandler.js
@@ -0,0 +1,16 @@
+import {createTodo, todoArray } from "./todoLogic"
+
+const titleInput = document.querySelector('#todo-title')
+const descriptionInput = document.querySelector('#todo-description')
+const dateInput = document.querySelector('input#todo-date')
+const submitButton = document.querySelector('.submit-button')
+
+function addNewTodo(newTodo){
+    todoArray.push(newTodo)
+}
+submitButton.addEventListener('click', ()=>{
+    if(!titleInput.value || !descriptionInput.value || !dateInput.value) return;
+    addNewTodo(createTodo(titleInput.value, descriptionInput.value, dateInput.value ))
+    
+    console.log(todoArray)
+})
