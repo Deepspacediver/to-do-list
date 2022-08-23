b2daff225acdbdfe0f9ba3271cd7398c67aacd88
deepspacediver <luka.koscik@gmail.com>
2022-08-23 16:12:06 +0200

    added function to create new Todo

--- src/todoLogic.js
+++ src/todoLogic.js
@@ -1,7 +1,15 @@
-export class todo{
+export let todoArray = []
+class todo{
     constructor(title, description, date){
-        this.title = title;
+        this.orignID = Date.now()
+        this.title = title
         this.description = description;
         this.date = date;
     }
+}
+    
+export function createTodo(titleInput, descriptionInput, dateInput){
+     const newTodo = new todo(titleInput, descriptionInput, dateInput)
+     return newTodo;
+
 }
