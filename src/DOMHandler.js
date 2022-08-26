1c1266b862a644a7d726c19f7ea340d610006f2f
deepspacediver <luka.koscik@gmail.com>
2022-08-26 21:32:23 +0200

    moved code to formHandler and added initial clear form function

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -26,34 +26,3 @@ export function createTodoDiv(todoObject){
     todoDate.textContent = `${todoObject.date}`
     todoWrapper.appendChild(todoDate)
 }
-
-const openFormButtons = document.querySelectorAll('[data-form-target]')
-const closeFormButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-console.log(openFormButtons)
-
-openFormButtons.forEach(button => {
-    button.addEventListener('click', ()=>{
-        const form = document.querySelector(button.dataset.formTarget)
-        openForm(form)
-    })
-})
-
-closeFormButtons.forEach(button => {
-    button.addEventListener('click', ()=>{
-        const form = button.closest('.todo-form-container')
-        closeForm(form)
-    })
-})
-
-function openForm(form){
-    if (form == null) return
-    form.classList.add('active')
-    overlay.classList.add('active')
-}
-
-function closeForm(form){
-    if (form == null) return
-    form.classList.remove('active')
-    overlay.classList.remove('active')
-}
--- src/formHandler.js
+++ src/formHandler.js
@@ -1,20 +1,62 @@
 import {todoArray, todo } from "./todoLogic"
-import { createTodoDiv } from "./DOMHandler"
+import { createTodoDiv,} from "./DOMHandler"
 
 const titleInput = document.querySelector('#todo-title')
 const descriptionInput = document.querySelector('#todo-description')
 const dateInput = document.querySelector('input#todo-date')
 const submitButton = document.querySelector('.submit-button')
 
-function addNewTodo(array,newTodo){
-    array.push(newTodo)
-}
-submitButton.addEventListener('click', ()=>{
+
+submitButton.addEventListener('click', (e)=>{
     if(!titleInput.value || !descriptionInput.value ||
          !dateInput.value) return;
          const newTodo = new todo(titleInput.value, descriptionInput.value, dateInput.value)  
          addNewTodo(todoArray, newTodo)
          createTodoDiv(newTodo)  
+         clearForm(submitButton)
+         closeForm(e.target.closest('.todo-form-container'))
     
 })
+
+const openFormButtons = document.querySelectorAll('[data-form-target]')
+const closeFormButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+console.log(openFormButtons)
+
+openFormButtons.forEach(button => {
+    button.addEventListener('click', ()=>{
+        const form = document.querySelector(button.dataset.formTarget)
+        openForm(form)
+    })
+})
+
+closeFormButtons.forEach(button => {
+    button.addEventListener('click', ()=>{
+        const form = button.closest('.todo-form-container')
+
+        closeForm(form)
+    })
+})
+
+function openForm(form){
+    if (form == null) return
+    form.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeForm(form){
+    if (form == null) return
+    form.classList.remove('active')
+    overlay.classList.remove('active')
+}
+function addNewTodo(array,newTodo){
+    array.push(newTodo)
+}
+function clearForm(button){
+    const inputContainers = Array.from(button.closest('div').querySelector('fieldset').children)
+    inputContainers.forEach(div => {
+        // div.first
+    })
+    console.log(inputContainers)
+}
 export {titleInput, descriptionInput, dateInput, submitButton}
